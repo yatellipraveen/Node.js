cf4f0ff3da03a876833e87c322e282936f6b72c8
yatellipraveen <yatellipraveen@gmail.com>
2020-04-13 19:01:14 +0530

    login and home page

--- facebook/facebook-ui/src/app/app.module.ts
+++ facebook/facebook-ui/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ facebook/facebook-ui/src/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms'
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm =new FormGroup({
+    username : new FormControl('',Validators.required),
+    password : new FormControl('',Validators.required)
+  });
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onLogin(){
+    this.loginForm.reset();
+  }
+
+}
